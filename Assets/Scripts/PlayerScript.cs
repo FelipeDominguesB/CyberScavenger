b9c6a9e4a7566496d1ab3f6714918166b84c4cf7
Felipe Domingues <felipedom2001@hotmail.com>
2024-04-13 13:34:33 -0300

    feat: update package version

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class PlayerScript : MonoBehaviour, IPlayer
 {
+    public Rigidbody character;
+    public float moveSpeed = 0.1F;
     public void ApplyDamage(int damage)
     {
         throw new System.NotImplementedException();
@@ -33,6 +36,31 @@ public class PlayerScript : MonoBehaviour, IPlayer
     // Update is called once per frame
     void Update()
     {
-        
+        float xMovement = 0;
+        float yMovement = 0;
+
+        int rotationAngle = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            xMovement -= moveSpeed;
+            rotationAngle += 180;
+
+            character.transform.position = new Vector3(character.transform.position.x + xMovement, character.transform.position.y + yMovement);
+            character.transform.eulerAngles = new Vector3(character.transform.rotation.x, character.transform.rotation.y, rotationAngle);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            xMovement += moveSpeed;
+            rotationAngle = 0;
+            character.transform.position = new Vector3(character.transform.position.x + xMovement, character.transform.position.y + yMovement);
+            character.transform.eulerAngles = new Vector3(character.transform.rotation.x, character.transform.rotation.y, rotationAngle);
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            this.ApplyDamage(1);
     }
 }
